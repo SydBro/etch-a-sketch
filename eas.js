b9a25dfdfaec84ed2a014f65c21fd5c90b03d26b
Sydney B <sydglyph@gmail.com>
2021-10-13 16:13:56 -0400

    made a script that generated the grid. made it so that each square in the grid changed color on mouse hover using an event listener. made a function that would drestroy the grid. wrote another event listener for the button to destroy the grid and create a new one to reset it.

--- eas.js
+++ eas.js
@@ -1,13 +1,39 @@
-const container = document.querySelector("#container")
-const square = document.createElement("div");
-square.classList.add("square");
-square.setAttribute("style", "background-color: aqua; height: 25px; width: 25px; margin: 1px");
-container.appendChild(square);
+function createGrid() {
+    const row = document.querySelector(".row");
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.setAttribute("style", "background-color: white; height: 25px; width: 25px;");
+    const container = document.querySelector("#container");
+    for (i=0; i<16; i++){
+        row.appendChild(square.cloneNode(true));
+    }
 
-function createDiv() {
     for (i=0; i<15; i++){
-        container.appendChild(square.cloneNode(true));
+        container.insertBefore(row.cloneNode(true), row);
     }
 }
 
-createDiv();
+function destroyGrid()
+{
+    const container = document.querySelector("#container");
+    const allRows = document.querySelectorAll(".row");
+    for (i=0; i<allRows.length; i++){
+        container.removeChild(allRows[i]);
+    }
+    const row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
+}
+
+createGrid();
+
+let hover = document.getElementById("container");
+hover.addEventListener("mouseover", function(e) {
+    e.target.style.backgroundColor = "black";
+});
+
+const resetButton = document.querySelector("#btn");
+resetButton.addEventListener("click", () => {
+    destroyGrid();
+    createGrid();
+});
